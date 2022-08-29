cc22312e2e05d387d979263e03b649474e0d5e08
JCL80 <jcambrac@gmail.com>
2022-08-29 02:21:43 -0400

    its not completed but im really tired

--- studybud/urls.py
+++ studybud/urls.py
@@ -1,10 +1,13 @@
-from xml.etree.ElementInclude import include
 from django.contrib import admin
+from django.conf import settings
 from django.urls import path , include
+from django.conf.urls.static import static
 
-
+from studybud.settings import MEDIA_ROOT
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('' , include('base.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL , document_root = settings.MEDIA_ROOT)
